4121708308312a35fc5a81113bf52d89a2047b7d
Cheleste1994 <wolverines@tut.by>
2023-04-13 14:32:15 +0300

    feat: task finish

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let rows = matrix.length
+  let cols = matrix[0].length
+  let result = []
+
+  for (let i = 0; i < rows; i++) {
+    const row = []
+
+    for (let j = 0; j < cols; j++) {
+      let count = 0
+
+      for (let x = i - 1; x <= i + 1; x++) {
+        for (let y = j - 1; y <= j + 1; y++) {
+          if (x >= 0 && x < rows && y >= 0 && y < cols && !(x === i && y === j)) {
+            if (matrix[x][y]) {
+              count++;
+            }
+          }
+        }
+      }
+      row.push(count);
+    }
+    result.push(row);
+  }
+
+  return result;
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function sortByHeight(arr) {
+
+// return arr.sort((a, b) => a == (-1) ? 0 : a < b ? -1 : 1)
+// }
+
+
+function sortByHeight(arr) {
+  let newArr = arr.filter(x => x !== -1).sort((a, b) => a - b);
+  console.log(newArr)
+  return arr.map( (x) => x !== -1 ? newArr.shift() : x );
 }
+
 
 module.exports = {
   sortByHeight
 };
+
