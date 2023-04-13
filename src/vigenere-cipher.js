024d7e99873fcb8e4f08a902d32b3020425b62a8
Cheleste1994 <wolverines@tut.by>
2023-04-13 16:18:14 +0300

    feat: task finish

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isRever = true) {
+    this.isRever = isRever;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    if (!text || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+   text = text.toUpperCase();
+   key = key.toUpperCase();
+   let result = '';
+   let j = 0;
+
+   for (let i = 0; i < text.length; i++) {
+  const textChar = text.charAt(i);
+  if (/^[A-Z]$/.test(textChar)) {
+    const keyChar = key.charAt(j % key.length);
+    const encryptedChar = String.fromCharCode(((textChar.charCodeAt(0) + keyChar.charCodeAt(0)) % 26) + 65);
+    console.log((textChar.charCodeAt(0) + keyChar.charCodeAt(0)) % 26)
+    result += encryptedChar;
+    j++;
+  } else {
+    result += textChar;
+     }
+    }
+
+    return this.isRever ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(text, key) {
+    if (!text || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    text = text.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let j = 0;
+    console.log(text)
+  for (let i = 0; i < text.length; i++) {
+  const encryptedChar = text.charAt(i);
+  if (/^[A-Z]$/.test(encryptedChar)) {
+    const keyChar = key.charAt(j % key.length);
+    const resultChar = String.fromCharCode(((encryptedChar.charCodeAt(0) - keyChar.charCodeAt(0) + 26) % 26) + 65);
+    result += resultChar;
+    j++;
+  } else {
+    result += encryptedChar;
+  }
+  }
+
+ return this.isRever ? result : result.split('').reverse().join('');
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
+const directMachine = new VigenereCipheringMachine();
+console.log(directMachine.encrypt('attack at dawn!', 'alphonse'))
